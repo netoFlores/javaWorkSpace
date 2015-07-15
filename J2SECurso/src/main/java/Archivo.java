681ecbb88639012a8806231295dbd7f510f7a266
netoFlores <ernesto.flores28@gmail.com>
2015-07-15 12:28:10 -0600

    Conectar Java con PostgreSQL

--- J2SECurso/src/main/java/Archivo.java
+++ J2SECurso/src/main/java/Archivo.java
@@ -1,5 +1,6 @@
 
 import java.io.File;
+import javax.swing.JOptionPane;
 
 
 public class Archivo {
@@ -7,7 +8,7 @@ public class Archivo {
     public static void main(String[] args) {
         File archivo = new File("\\home\\neto\\ejemplo");
         boolean  respuesta = archivo.delete();//archivo.mkdir() para crear carpetas.
-        
+        JOptionPane.showMessageDialog(null, "Hola mundo");
         System.out.println(respuesta);
     }
 }
